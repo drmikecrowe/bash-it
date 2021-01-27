583c8606431ba36a2888bd37e6466936a77fc815
Mike Crowe <drmikecrowe@gmail.com>
2021-01-27 06:18:48 -0500

    Addinb backup and basher

--- plugins/available/basher.plugin.bash
+++ plugins/available/basher.plugin.bash
@@ -1,10 +1,16 @@
+# shellcheck shell=bash
 cite about-plugin
-about-plugin 'load dotbare, if you are using it: https://github.com/kazhala/dotbare'
+about-plugin 'initializes basher, the shell package manager'
 
-DIR="$HOME/.basher"
-if [[ -d "$DIR" ]]; then
-	if [[ ":$PATH:" != *"$DIR/bin:"* ]]; then
-		export PATH="$PATH:$DIR/bin"
-		eval "$(basher init - bash)"
+# https://github.com/basherpm/basher
+
+if ! _command_exists basher; then
+	if [[ -x "$HOME/.basher/bin/basher" ]]; then
+		pathmunge "$HOME/.basher/bin"
+	else
+		_log_warning 'basher not found'
+		return 0
 	fi
 fi
+
+eval "$(basher init - bash)"
